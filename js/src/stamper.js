08ef2e70ab5b40e71c009b481fbb32cdeb4a481d
Dave Ross <dave@davidmichaelross.com>
2016-02-10 23:54:07 -0500

    Built out a theme

--- js/src/stamper.js
+++ js/src/stamper.js
@@ -162,4 +162,22 @@
      download(canvas.toDataURL('image/png'), 'stamp.png', 'image/png'); 
   });
   
+  var inputs = document.querySelectorAll( 'input[type=file]' );
+    Array.prototype.forEach.call( inputs, function( input ) {
+        var label	 = input.nextElementSibling,
+            labelVal = label.innerHTML;
+
+        input.addEventListener( 'change', function( e ) {
+            var fileName = '';
+            if( this.files && this.files.length > 1 )
+                fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+            else
+                fileName = e.target.value.split( '\\' ).pop();
+
+            if( fileName )
+                label.querySelector( 'span' ).innerHTML = fileName;
+            else
+                label.innerHTML = labelVal;
+        });
+    });
 }());
